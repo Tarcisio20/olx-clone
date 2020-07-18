84783546d763161b593dc151e6729df43ec25dbe
Tarcisio Silva <tarcisior19@hotmail.com>
2020-07-18 12:17:48 -0300

    Subindo tudo para o Git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
+import { Template } from './Components/mainComponents'
+import  Header  from './Components/Partials/Header'
+import  Footer  from './Components/Partials/Footer'
+
+import Router from './Router'
+
+import './App.css'
+
+function Page(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Template>
+          <Header />
+          <Router />
+          <Footer />
+        </Template>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    user:state.user
+  }
+}
+
+const mapDispatchToProps = dispath => {
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
---
+++ src/Pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Page = () => {
+    return (
+        <>
+            <div>Pagina não encontrada!</div>
+            <Link to="/">Voltar para a home.</Link>
+        </>
+    )
+}
+
+export default Page
