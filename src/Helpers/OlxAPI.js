86ab03fc4cb5f7dbb03f970fafbc7ff95a2269c9
Tarcisio Silva <tarcisior19@hotmail.com>
2020-07-22 13:06:18 -0300

    Implementação da tela de Cadastro

--- src/Helpers/OlxAPI.js
+++ src/Helpers/OlxAPI.js
@@ -49,6 +49,15 @@ const OlxAPI = {
         const json = await apiFetchPost('/user/signin', {email, password}
         )
         return json
+    },
+    register: async (name, stateLoc, email, password) => {
+        const json = await apiFetchPost('/user/signup', {name, email, password, state:stateLoc}
+        )
+        return json
+    },
+    getStates:async () => {
+        const json = await apiFetchGet('/states')
+        return json.states
     }
 }
 
